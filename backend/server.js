9b6ef9aebdf393016e480767f29850d0d9e73b42
andreysouza333 <andreybulcao@gmail.com>
2026-09-21 20:15:19 +0000

    atividade continuada

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,33 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res) => {
 
-res.status(200).json({
-    mensagem:"API Funcioando com sucesso"
+app.get("/", (req, res) => {
+    res.status(200).json({
+        mensagem: "API Funcionando com sucesso"
+    });
 });
-});
 
-app.listen(3000,()=>{
-    console.log("Servidor rodando na porta 3000");
+app.get("/items", (req, res) => {
+    const items = [
+        {
+            nome: "Notebook",
+            preco: 3000,
+            categoria: "Eletronico"
+        },
+        {
+            nome: "Processador",
+            preco: 700,
+            categoria: "Eletronico"
+        }
+    ];
 
-app.get("/items",(req,res)=>{
-const items =[
-    {
-        nome:"Notebook",
-        preço: 3000,
-        Categoria: "Eletronico"
-    },
-    {
-        nome:"Processador",
-        preço: 700,
-        Categoria: "Eletronico"
-    }
-];
     res.status(200).json(items);
-        
 });
 
-});              
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
